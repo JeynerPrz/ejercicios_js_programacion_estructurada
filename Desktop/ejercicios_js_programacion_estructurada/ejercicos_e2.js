81550c940056688b1d3a964fcee22d4f2d077497
jeynerPrz <jeynerperezalvarez@gmail.com>
2025-08-14 23:07:07 -0600

    ejercicios

---
+++ Desktop/ejercicios_js_programacion_estructurada/ejercicos_e2.js
@@ -0,0 +1,33 @@
+
+
+let nombre = "jeyner perez alvarez";
+console.log(nombre)
+
+// Declara dos variables edad y ciudad y muéstralas juntas en un solo console.log.
+
+let edad = 21;
+let ciudad = "San carlos";
+console.log
+
+
+// 5.
+
+let esMayorDeEdad = true;
+console.log("¿es mayor de edad?"  + esMayorDeEdad);
+
+//6.
+let n1 = 15;
+let n2 = 25;
+let suma = n1 + n2;
+console.log(suma);
+
+// 7.
+
+let num1  = 100;
+let num2  = 20;
+
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+
+console,log("resta: $ { }")
